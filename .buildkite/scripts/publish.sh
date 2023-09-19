16d6f2dbc9466d15b4fa0691ccf5112fabec18f5
Davide Girardi <1390902+girodav@users.noreply.github.com>
2023-09-19 15:46:45 +0100

    Enable DRA publishing (#325)

--- .buildkite/scripts/publish.sh
+++ .buildkite/scripts/publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 
 WORKFLOW=$1
 # The "branch" here selects which "$BRANCH.gradle" file of release manager is used
@@ -28,21 +28,20 @@ chmod a+w build/distributions
 echo "Retrieving DRA crededentials..."
 DRA_CREDS=$(vault kv get -field=data -format=json kv/ci-shared/release/dra-role)
 
-# TODO: Enable as soon as everything is in place to publish artifacts to DRA
 # Run release-manager
-#echo "Running release-manager container..."
-#IMAGE="docker.elastic.co/infra/release-manager:latest"
-#docker run --rm \
-#  --name release-manager \
-#  -e VAULT_ADDR=$(echo $DRA_CREDS | jq -r '.vault_addr') \
-#  -e VAULT_ROLE_ID=$(echo $DRA_CREDS | jq -r '.role_id') \
-#  -e VAULT_SECRET_ID=$(echo $DRA_CREDS | jq -r '.secret_id') \
-#  --mount type=bind,readonly=false,src="${PWD}",target=/artifacts \
-#  "$IMAGE" \
-#    cli collect \
-#      --project assetbeat \
-#      --branch "${BRANCH}" \
-#      --commit "${BUILDKITE_COMMIT}" \
-#      --workflow "${WORKFLOW}" \
-#      --version "${VERSION}" \
-#      --artifact-set main
+echo "Running release-manager container..."
+IMAGE="docker.elastic.co/infra/release-manager:latest"
+docker run --rm \
+  --name release-manager \
+  -e VAULT_ADDR=$(echo $DRA_CREDS | jq -r '.vault_addr') \
+  -e VAULT_ROLE_ID=$(echo $DRA_CREDS | jq -r '.role_id') \
+  -e VAULT_SECRET_ID=$(echo $DRA_CREDS | jq -r '.secret_id') \
+  --mount type=bind,readonly=false,src="${PWD}",target=/artifacts \
+  "$IMAGE" \
+    cli collect \
+      --project assetbeat \
+      --branch "${BRANCH}" \
+      --commit "${BUILDKITE_COMMIT}" \
+      --workflow "${WORKFLOW}" \
+      --version "${VERSION}" \
+      --artifact-set main
